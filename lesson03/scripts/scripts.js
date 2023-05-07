0aeda67df59c796e7d36f75d540ad1a25edef386
nomorezhanje23 <nomorezhanje100@gmail.com>
2023-05-07 07:56:24 +0200

    week 3

---
+++ lesson03/scripts/scripts.js
@@ -0,0 +1,7 @@
+const today = new Date();
+const year = today.getFullYear();
+const msg = `&copy;  ${year}  .:| Nomore T. Zhanje |:. Cape Town <br/>
+<b>last modified: </b>${document.lastModified}`;
+
+const el = document.getElementById("myfooter");
+el.innerHTML = msg;
