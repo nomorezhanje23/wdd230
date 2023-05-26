805da9ad934d5240a8c28d1ddc10b81dcbda4911
nomorezhanje23 <nomorezhanje23@gmail.com>
2023-05-26 22:32:01 +0200

    Lesson 4 done

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -1,7 +1,8 @@
 const today = new Date();
 const year = today.getFullYear();
-const msg = `&copy;  ${year}  .:| Cape Town Chambers .:|:. Nomore T. Zhanje |:. WDD230
+const msg = `&copy;  ${year}  .:| Nomore T. Zhanje |:. WDD230 .:| Cape Town Chamber of Commerce <br/>
 <b>last modified: </b>${document.lastModified}`;
 
 const el = document.getElementById("myfooter");
 el.innerHTML = msg;
+
