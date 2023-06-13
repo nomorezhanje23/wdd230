c4973f4c1c8c800e431c8fd91bbef80de9e4ee45
nomorezhanje23 <nomorezhanje23@gmail.com>
2023-06-13 23:08:15 +0200

    fixed the discover page

--- discover07/scripts/dates.js
+++ discover07/scripts/dates.js
@@ -1,43 +1,65 @@
-/*select the DOM elements to manipulate (we will output to these)*/
-const datefield = document.querySelector("time");
-// for european/family history format with day first.
-const datefieldUK = document.querySelector("aside");
-//const datemessage = document.querySelector("p");
+const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-// derive the current date using a date object
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(now);
-// "full" or long, medium, short options ... try them
+const imgOptions = {
+  threshold: 0, 
+  rootMargin: "0px 0px 50px 0px"
+};
 
-//datefield.textContent = fulldate;
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
-//datemessage.innerHTML = `Today is <strong>${fulldate}</strong>. What are you going to do?`;
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
 
 const today = new Date();
 const year = today.getFullYear();
 
-const msg = `&copy;  ${year}  .:| Cape Town Chambers .:|:. Nomore T. Zhanje |:. WDD230
+const msg = `&copy;  ${year}  .:| Cape Town Chamber .:|:. Nomore T. Zhanje |:. WDD230
 <b>last modified: </b>${document.lastModified}`;
 
 const el = document.getElementById("myfooter");
 el.innerHTML = msg;
 
-function newDate(){
-    return new Date();
+const userVisit  = document.querySelector(".user-visit"); //class of HTML
+
+//VARIABLES
+let lastVisitTime = localStorage.getItem("visits-localstorage"); //LAST VISIT
+let currentTimestamp = Date.now(); //CURRENT DATE IN MILLISECONDS
+const msForDay = 86400000; //MILLISECONDS IN A DAY
+const lastVisitInDays = Math.floor(currentTimestamp - lastVisitTime) / msForDay; //DIFERENCE IN DAYS
+
+localStorage.setItem("visits-localstorage", currentTimestamp); //UPDATE LAST VISIT
+
+ function calculateDays() {
+    lastVisitInDays;
+    const message = `| 🤗 Welcome back! Your last visit was ${lastVisitInDays} days ago |`;
+    return message;
 }
 
-const jour = newDate().getDay();
-
-console.log(jour);
-
-document.addEventListener("DOMContentLoaded", function(){
-    if (jour === 1 || jour === 2) {
-        const element = document.getElementById('banner');
-        element.style.display = 'block';
-    }
-})
+if (lastVisitInDays >= 1) {
+    userVisit.textContent = calculateDays();
+    } 
+else {
+        userVisit.textContent = ` |  😎 Welcome to The Cape Town Chamber!  This is your first visit  😎 |`;
+     }
--- discover07/scripts/menu.js
+++ discover07/scripts/menu.js
@@ -1,3 +1,4 @@
 function toggleMenu(){
     document.getElementById("menu").classList.toggle("primaryNav");
 }
+
