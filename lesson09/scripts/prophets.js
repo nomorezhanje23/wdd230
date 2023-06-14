96cef568c4c71ef6362b26ab2d1ce11b6ee9d112
nomorezhanje23 <nomorezhanje23@gmail.com>
2023-06-15 00:18:38 +0200

    changed the view

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,5 +1,6 @@
-const url =
-  "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
+const url ="https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
+
+//first part from the  text
 
 async function getProphetData() {
   const response = await fetch(url);
@@ -17,68 +18,67 @@ const displayProphets = (prophets) => {
     // Create elements to add to the div.cards element
     let card = document.createElement("section");
     let h2 = document.createElement("h2");
-    let daybirth = document.createElement("p");
-    let birthplace = document.createElement("p");
-    let death = document.createElement("p");
-    let age = document.createElement("p");
+    //notes from bro blazzard JSON
+    let daybirth = document.createElement("text");
+    let birthplace = document.createElement("text");
+    let deathYear = document.createElement("text");
+    let age = document.createElement("text");
+
     let portrait = document.createElement("img");
 
-    // Estract prophet's date of birth and death
-    let dob = `${prophet.birthdate}`;
-    let dod = `${prophet.death}`;
+    //dates of birth and death of the prophets from the function above
+    let dateofbirth = `${prophet.birthdate}`;
+    let dateofdirth = `${prophet.death}`;
 
-    // Get year of prophet's birth and death
-    let birthyear = dob.slice(-4);
-    let deathyear = dod.slice(-4);
+    // retrieving the year only from the date string[ use .slice(-4) to extract the last four characters of the string.]
+    let birthyear = dateofbirth.slice(-4);
+    let deathyear = dateofdirth.slice(-4);
 
-    // Compute prophet's age
+    // Getting the prophet's age; death/current year - birth year
     let prophetAge = Number(deathyear) - Number(birthyear);
 
-    // Add class phophet-card to the section element
+     //code to position the card and the potrait vertically
     card.classList.add("prophet-card");
-
-    // Add class prophet-img to the image element
     portrait.classList.add("prophet-img");
 
-    // Build the h2, Date of Birth, Place of Birth, Date of Death, and age content
+    // Information to be displayed
     h2.textContent = `${prophet.name} ${prophet.lastname}`;
-    daybirth.textContent = `Date of Birth: ${dob}`;
+    daybirth.textContent = `Date of Birth: ${dateofbirth}`;
     birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
-    death.textContent = `Date of Death: ${dod}`;
+    deathYear.textContent = `Time of Death: ${dateofdirth}`;
     age.textContent = `Died at Age: ${prophetAge}`;
-
+//-----------------------------------Notes from activity ---------------------------------
     // Build the image portrait by setting all the relevant attribute
+
     portrait.setAttribute("src", prophet.imageurl);
-    portrait.setAttribute(
-      "alt",
-      `Portrait of ${prophet.name} ${prophet.lastname}`
-    );
+    portrait.setAttribute("alt",`Portrait of ${prophet.name} ${prophet.lastname}`);
     portrait.setAttribute("loading", "lazy");
     portrait.setAttribute("width", "340");
     portrait.setAttribute("height", "440");
 
-    // Append the section(card) with the created elements
+    //From the notes Append the section(card) with the created elements
     cards.appendChild(card);
     card.appendChild(h2);
     card.appendChild(daybirth);
     card.appendChild(birthplace);
 
-    // Get current year
-    const d = new Date();
-    let currentYear = d.getFullYear();
+    // Get current year T(now)
+    const nowT = new Date();
+    let currentYear = nowT.getFullYear();
 
-    // Compute current age for the living prophet
+    // The age of the thd Prophet
     let currentAge = Number(currentYear) - Number(birthyear);
 
     // Display the date of death and age for the deceased prophets
-    if (death.textContent !== `Date of Death: null`) {
-      card.appendChild(death);
+    if (deathYear.textContent !== `Time of Death: null`) {
+      card.appendChild(deathYear);
       card.appendChild(age);
     } else {
       // Display the age of the living prophet
-      age.textContent = ` Age: ${currentAge}`;
+      age.textContent = ` Current Age: ${currentAge}`;
       card.appendChild(age);
     }
     card.appendChild(portrait);
   });
 };
+ 
