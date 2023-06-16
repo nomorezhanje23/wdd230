413dcd842e27842c0c8e39ec29e56cd6eaa5556f
nomorezhanje23 <nomorezhanje23@gmail.com>
2023-06-17 01:38:46 +0200

    testing Json

--- directory09/scripts/directory.js
+++ directory09/scripts/directory.js
@@ -1,58 +1,58 @@
 //start by the url of the JSON file
-const url ="https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
+const url ="https://nomorezhanje23.github.io/wdd230/directory09/data.json";
 
 //first part from the  text
 
-async function getProphetData() {
+async function getCompanyData() {
   const response = await fetch(url);
   const data = await response.json();
   //console.table(data.prophets);
-  displayProphets(data.prophets);
+  displayCompanies(data.companies);
 }
 
-getProphetData();
+getCompanyData();
 
-const displayProphets = (prophets) => {
+const displayCompanies = (companies) => {
   const cards = document.querySelector("div.cards");
 
-  prophets.forEach((prophet) => {
+  companies.forEach((company) => {
     // Create elements to add to the div.cards element
     let card = document.createElement("section");
     let h2 = document.createElement("h2");
-    //notes from bro blazzard JSON
-    let daybirth = document.createElement("text");
-    let birthplace = document.createElement("text");
-    let deathYear = document.createElement("text");
-    let age = document.createElement("text");
+    //notes from my JSON
+    let address = document.createElement("text");
+    let phone = document.createElement("text");
+    let website = document.createElement("text");
+    let membership = document.createElement("text");
 
     let portrait = document.createElement("img");
 
     //dates of birth and death of the prophets from the function above
-    let dateofbirth = `${prophet.birthdate}`;
-    let dateofdirth = `${prophet.death}`;
+    let dateofbirth = `${company.birthdate}`;
+    let dateofdirth = `${company.death}`;
 
     // retrieving the year only from the date string[ use .slice(-4) to extract the last four characters of the string.]
-    let birthyear = dateofbirth.slice(-4);
-    let deathyear = dateofdirth.slice(-4);
+    //let birthyear = dateofbirth.slice(-4);
+    //let deathyear = dateofdirth.slice(-4);
 
     // Getting the prophet's age; death/current year - birth year
    // let prophetAge = Number(deathyear) - Number(birthyear);
 
      //code to position the card and the potrait vertically
-    card.classList.add("prophet-card");
-    portrait.classList.add("prophet-img");
+    card.classList.add("company-card");
+    portrait.classList.add("company-img");
 
     // Information to be displayed after validating the conditions below
-    h2.textContent = `${prophet.name} ${prophet.lastname}`;
-    daybirth.textContent = `Date of Birth: ${dateofbirth}`;
-    birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
-    deathYear.textContent = `Time of Death: ${dateofdirth}`;
-    //age.textContent = `Died at Age: ${prophetAge}`;
+    h2.textContent = `${company.companyname}`;
+    address.textContent = `Adress: ${address}`;
+    phone.textContent = `Contact: ${prophet.phone}`;
+    website.textContent = `More info: ${website}`;
+    membership.textContent = `Chamber Membership: ${membership}`;
 //-----------------------------------Notes from activity ---------------------------------
     // Build the image portrait by setting all the relevant attribute
 
-    portrait.setAttribute("src", prophet.imageurl);
-    portrait.setAttribute("alt",`Portrait of ${prophet.name} ${prophet.lastname}`);
+    portrait.setAttribute("src", company.imageurl);
+    portrait.setAttribute("alt",`Portrait of ${company.companyname}`);
     portrait.setAttribute("loading", "lazy");
     portrait.setAttribute("width", "340");
     portrait.setAttribute("height", "440");
@@ -60,8 +60,11 @@ const displayProphets = (prophets) => {
     //From the notes Append the section(card) with the created elements
     cards.appendChild(card);
     card.appendChild(h2);
-    card.appendChild(daybirth);
-    card.appendChild(birthplace);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(website);
+    card.appendChild(membership);
+
 
     // Get current year T(now)
     const nowT = new Date();
