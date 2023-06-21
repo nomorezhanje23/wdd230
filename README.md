b25774be884e2e2b9f415340536a439a2910dd38
nomorezhanje23 <nomorezhanje23@gmail.com>
2023-06-21 21:50:53 +0200

    reset

--- README.md
+++
@@ -1,2 +0,0 @@
-# wdd230
-GitHub for wdd230 (web development front end 1) semester 4 @BYU-Idaho 
--- lesson02/scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-const today = new Date();
-const year = today.getFullYear();
-const msg = `&copy;  ${year}  .:| Nomore T. Zhanje |:. Cape Town <br/>
-<b>last modified: </b>${document.lastModified}`;
-
-const el = document.getElementById("myfooter");
-el.innerHTML = msg;
