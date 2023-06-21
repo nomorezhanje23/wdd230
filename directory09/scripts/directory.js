31fb5e359ae9813631888df2065634e33a71e0fb
nomorezhanje23 <nomorezhanje23@gmail.com>
2023-06-21 21:33:34 +0200

    discover page

--- directory09/scripts/directory.js
+++ directory09/scripts/directory.js
@@ -1,12 +1,10 @@
 //start by the url of the JSON file
 const url ="https://nomorezhanje23.github.io/wdd230/directory09/data.json";
 
-//first part from the  text
-
-async function getCompanyData() {
+async function getCompanyData(){
   const response = await fetch(url);
   const data = await response.json();
-  //console.table(data.prophets);
+
   displayCompanies(data.companies);
 }
 
@@ -16,72 +14,46 @@ const displayCompanies = (companies) => {
   const cards = document.querySelector("div.cards");
 
   companies.forEach((company) => {
-    // Create elements to add to the div.cards element
+    
     let card = document.createElement("section");
-    let h2 = document.createElement("h2");
-    //notes from my JSON
+    let h4 = document.createElement("h4");
+    //parts from my code
+    let portrait = document.createElement("ïmg");
+    let companyname = document.createElement("text");
     let address = document.createElement("text");
     let phone = document.createElement("text");
     let website = document.createElement("text");
     let membership = document.createElement("text");
 
-    let portrait = document.createElement("img");
-
-    //dates of birth and death of the prophets from the function above
-    let dateofbirth = `${company.birthdate}`;
-    let dateofdirth = `${company.death}`;
-
-    // retrieving the year only from the date string[ use .slice(-4) to extract the last four characters of the string.]
-    //let birthyear = dateofbirth.slice(-4);
-    //let deathyear = dateofdirth.slice(-4);
-
-    // Getting the prophet's age; death/current year - birth year
-   // let prophetAge = Number(deathyear) - Number(birthyear);
-
-     //code to position the card and the potrait vertically
     card.classList.add("company-card");
     portrait.classList.add("company-img");
+    //what will show on the display
 
-    // Information to be displayed after validating the conditions below
-    h2.textContent = `${company.companyname}`;
-    address.textContent = `Adress: ${address}`;
-    phone.textContent = `Contact: ${prophet.phone}`;
-    website.textContent = `More info: ${website}`;
-    membership.textContent = `Chamber Membership: ${membership}`;
-//-----------------------------------Notes from activity ---------------------------------
-    // Build the image portrait by setting all the relevant attribute
+    h4.textContent =`${company.companyname}`;
+    companyname.textContent = `${company.companyname}`;
+    address.textContent = `${company.address}`;
+    phone.textContent = `${company.phone}`;
+    website.textContent = `${company.website}`;
+    membership.textContent = `${company.membership}`;
+
+    //Building the image portrait by setting all the relevant attribute
 
     portrait.setAttribute("src", company.imageurl);
     portrait.setAttribute("alt",`Portrait of ${company.companyname}`);
     portrait.setAttribute("loading", "lazy");
+    website.setAttribute("href", `${company.website}`);
+    website.setAttribute("target", "_blank");
     portrait.setAttribute("width", "340");
     portrait.setAttribute("height", "440");
 
-    //From the notes Append the section(card) with the created elements
     cards.appendChild(card);
-    card.appendChild(h2);
+    card.appendChild(h4);
+    //might need a code here! Lets see
     card.appendChild(address);
     card.appendChild(phone);
     card.appendChild(website);
     card.appendChild(membership);
 
-
-    // Get current year T(now)
-    const nowT = new Date();
-    let currentYear = nowT.getFullYear();
-
-    // The age of the the Prophet
-    let currentAge = Number(currentYear) - Number(birthyear);
-
-    // Display the time of death and age for the deceased prophets
-    if (deathYear.textContent !== `Time of Death: null`) {
-      card.appendChild(deathYear);
-      card.appendChild(age);
-    } else {
-      // Display the age of the living prophet
-      age.textContent = ` Current Age: ${currentAge}`;
-      card.appendChild(age);
-    }
     card.appendChild(portrait);
   });
 };
--- directory09/scripts/menu.js
+++ directory09/scripts/menu.js
@@ -1,3 +1,42 @@
 function toggleMenu(){
     document.getElementById("menu").classList.toggle("primaryNav");
 }
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The code for the grid and list views
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
+
+let lastVisitTime = localStorage.getItem("visits-localstorage"); //LAST VISIT
+let currentTimestamp = Date.now(); //CURRENT DATE IN MILLISECONDS
+const msForDay = 86400000; //MILLISECONDS IN A DAY
+const lastVisitInDays = Math.floor((currentTimestamp - lastVisitTime) / msForDay); //DIFERENCE IN DAYS
+
+localStorage.setItem("visits-localstorage", currentTimestamp); //UPDATE LAST VISIT
+
+ function calculateDays() {
+    lastVisitInDays;
+    const message = `| 🤗 Welcome back! Your last visit was ${lastVisitInDays} days ago |`;
+    return message;
+}
+
+if (lastVisitInDays >= 1) {
+    userVisit.textContent = calculateDays();
+    } 
+else {
+        userVisit.textContent = ` |  😎 Welcome to The Cape Town Chamber!  This is your first visit  😎 |`;
+     }
