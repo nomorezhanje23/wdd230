819d6a9cf1aaced1f80e72b6b9163b9823233738
nomorezhanje23 <nomorezhanje23@gmail.com>
2023-07-07 03:26:32 +0200

    directory is done

--- directory09/scripts/directory.js
+++ directory09/scripts/directory.js
@@ -2,59 +2,67 @@
 
 const url ="https://nomorezhanje23.github.io/wdd230/directory09/data.json";
 
-async function getCompanyData(){
-  const response = await fetch(url);
-  const data = await response.json();
+fetch(url)
+  .then(function(response){
+    return response.json();
+})
+.then(function(jsonObject){
+  const companies = jsonObject['companies'];
+  
+  for( let i =0; i < companies.length; i++){
 
-  displayCompanies(data.companies);
+    let image = document.createElement('img');
+    let grid = document.createElement('section');
+    let companyname = document.createElement('h2');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let website = document.createElement('a');
+    let membership = document.createElement('p')
+    
+    
+  
+    var altImg = companies[i].companyname ;
+  
+    companyname.textContent = companies[i].companyname;
+    address.textContent = 'Adrress: '+ companies[i].address;
+    phone.textContent = 'Tel: ' + companies[i].phone;
+    image.setAttribute('src', companies[i].imageurl);
+    membership.textContent = `${companies[i].membership}`;
+    website.textContent = `${companies[i].website}`;
+    website.setAttribute('href', `${companies[i].website}`);
+  
+    website.href = 'website'; 
+
+    grid.appendChild(companyname); 
+    grid.appendChild(address);  
+    grid.appendChild(phone);
+    grid.appendChild(image, image.alt = altImg);
+    grid.appendChild(website, website.href); 
+    grid.appendChild(membership);
+
+    document.querySelector("article.grid").appendChild(grid);
+   
+   }
+  
+  });
+
+
+//responsive layouts
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+listbutton.addEventListener("click", showList); 
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
 }
 
-getCompanyData();
 
-const displayCompanies = (companies) => {
-  const cards = document.querySelector("div.cards");
 
-  companies.forEach((company) => {
-    
-    let card = document.createElement("section");
-    let h4 = document.createElement("h4");
-    //parts from my code
-    let portrait = document.createElement("img");
-    let companyname = document.createElement("text");
-    let address = document.createElement("text");
-    let phone = document.createElement("text");
-    let website = document.createElement("text");
-    let membership = document.createElement("text");
-
-    card.classList.add("company-card");
-    portrait.classList.add("company-img");
-    //what will show on the display
-
-    h4.textContent =`${company.companyname}`;
-    companyname.textContent = `${company.companyname}`;
-    address.textContent = `${company.address}`;
-    phone.textContent = `${company.phone}`;
-    website.textContent = `${company.website}`;
-    membership.textContent = `${company.membership}`;
-
-    //Building the image portrait by setting all the relevant attribute
-
-    portrait.setAttribute("src", company.imageurl);
-    portrait.setAttribute("alt",`Portrait of ${company.companyname}`);
-    portrait.setAttribute("loading", "lazy");
-    website.setAttribute("href", `${company.website}`);
-    website.setAttribute("target", "_blank");
-    //portrait.setAttribute("width", "340");
-    //portrait.setAttribute("height", "440");
-
-    cards.appendChild(card);
-    card.appendChild(h4);
-    //might need a code here! Lets see
-    card.appendChild(address);
-    card.appendChild(phone);
-    card.appendChild(website);
-    card.appendChild(membership);
-
-    card.appendChild(portrait);
-  });
-};
